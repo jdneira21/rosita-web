8fb17b257f9adc54e05552ff63174d0dc8bdfa01
Junior <jdneira21@gmail.com>
2022-11-15 04:04:19 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
-});
+  site: 'https://jdneira21.github.io',
+  base: '/rosita-web',
+})
