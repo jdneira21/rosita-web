e5724199218835c3be679d9ea04b1a579dd4bcf9
Junior <jdneira21@gmail.com>
2022-11-15 10:51:40 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,12 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
   site: 'https://jdneira21.github.io',
   base: '/rosita-web',
+  integrations: [tailwind({
+    // Example: Disable injecting a basic `base.css` import on every page.
+    // Useful if you need to define and/or import your own custom `base.css`.
+    config: { applyBaseStyles: false }
+  })],
 })
+
