d7f7cbfc5063b0aaf03819e3ae676d64f84e3a0b
Junior <jdneira21@gmail.com>
2022-11-15 11:10:25 -0500

    cfg

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,57 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
+// import sitemap from '@astrojs/sitemap';
+// import image from '@astrojs/image';
+// import mdx from '@astrojs/mdx';
+// import partytown from '@astrojs/partytown';
+
+// import { remarkReadingTime } from './src/utils/frontmatter.js';
+
+import { SITE } from './src/config.mjs';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jdneira21.github.io',
-  base: '/rosita-web',
-  integrations: [tailwind({
-    // Example: Disable injecting a basic `base.css` import on every page.
-    // Useful if you need to define and/or import your own custom `base.css`.
-    config: { applyBaseStyles: false }
-  })],
-})
+  // Astro uses this full URL to generate your sitemap and canonical URLs in your final build
+  site: SITE.origin,
+  base: SITE.basePathname,
+  trailingSlash: SITE.trailingSlash ? 'always' : 'never',
 
+  output: 'static',
+
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    // sitemap(),
+    // image({
+    //   serviceEntryPoint: '@astrojs/image/sharp',
+    // }),
+    // mdx(),
+
+    /* Disable this integration if you don't use Google Analytics (or other external script). */
+    // partytown({
+    //   config: { forward: ['dataLayer.push'] },
+    // }),
+  ],
+
+  // markdown: {
+  //   remarkPlugins: [remarkReadingTime],
+  //   extendDefaultPlugins: true,
+  // },
+
+  vite: {
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, './src'),
+      },
+    },
+  },
+});
